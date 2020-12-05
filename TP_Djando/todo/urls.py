196ad5ecbfa5594ddad26fa3e3b15854c4674715
Moussa <moussainssa@outlook.fr>
2020-12-05 21:24:24 +0100

    Update urls.py

--- TP_Djando/todo/urls.py
+++ TP_Djando/todo/urls.py
@@ -3,8 +3,4 @@ from todo import views
 
 urlpatterns = [
     path('', views.index, name='index'),  
-    path('addnew',views.addnew),  
-    path('edit/<int:id>', views.edit),  
-    path('update/<int:id>', views.update),  
-    path('delete/<int:id>', views.destroy),  
 ]
